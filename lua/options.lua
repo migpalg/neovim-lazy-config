1e2c7f8711f451952d3f2336c0a54c8880c86674
Miguel Palacio <migpalg@gmail.com>
2025-02-16 15:19:47 -0500

    feat: initial commit

---
+++ lua/options.lua
@@ -0,0 +1,17 @@
+-- default tab as 2 spaces
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+
+-- prevent line wrap
+vim.o.wrap = false
+
+-- show line numbers
+vim.o.number = true
+vim.o.relativenumber = true
+
+
+-- set coloscheme
+vim.cmd.colorscheme("catppuccin")
+
